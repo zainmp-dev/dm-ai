932c8b4729f41a396291a194001a5208aee97591
Shifna Riya <shifnath369@gmail.com>
2026-06-17 19:15:41 +0530

    search bar updated

--- modules/blog/blog-views.tsx
+++ modules/blog/blog-views.tsx
@@ -540,9 +540,9 @@ export function BlogList() {
 
   return (
     <div className="space-y-4">
-      <div className="relative max-w-md">
+      <div className="relative w-full">
         <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
-        <Input value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search blogs..." className="pl-9" />
+        <Input value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search blogs..." className="w-full pl-9" />
       </div>
 
       <div className="flex flex-wrap items-center justify-between gap-3">
