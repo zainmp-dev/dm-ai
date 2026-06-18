8ad0cde66858273163094ac039238b9f0646322a
Shifna Riya <shifnath369@gmail.com>
2026-06-18 13:46:23 +0530

    Add icon variant to button component for enhanced styling options

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-lg px-3",
         lg: "h-11 px-6",
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
